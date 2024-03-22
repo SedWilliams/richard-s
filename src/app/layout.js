6e9fb28f068c50fe3de66b10bbd2b85ea824ad04
tre3iii <lilshed3@gmail.com>
2024-03-21 22:57:07 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <!-- pass all other layoits and page returns as children -->
       <body className={inter.className}>{children}</body>
     </html>
   )
